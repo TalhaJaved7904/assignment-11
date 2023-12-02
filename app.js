a9d3982a8fba0adfa6cd36cf9a31b999b390b6f1
TalhaJaved7904 <talhamuhammadjaved@gmail.com>
2023-12-02 08:44:57 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,11 @@
+var totalmarks = 500
+
+var a =parseInt(prompt("enter the marks of COMPUTER "))
+var b =parseInt(prompt("enter the marks of SCIENCE "))
+var c =parseInt(prompt("enter the marks of ENGLISH "))
+var d =parseInt(prompt("enter the marks of URDU "))
+var e =parseInt(prompt("enter the marks of MATHS "))
+var f =(a + b + c + d + e)
+alert(f)
+var percentage = (f * 100)/ totalmarks
+alert(percentage)
